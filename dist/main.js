8fbffca9adca767e3524269cba0e07841fa405a5
m05e5 <calebmoses1111@gmail.com>
2021-08-09 16:37:41 +0100

    configure webpack and jest

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('hello world')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG9CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vanNfY2Fwc3RvbmUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoJ2hlbGxvIHdvcmxkJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
